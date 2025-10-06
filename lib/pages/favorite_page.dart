9f29006b04d5a9907ac97da0ae95c70dc86a7234
leloeduk <leloeduk2025@gmail.com>
2025-10-06 23:14:39 +0100

    feat: add favorite_page

---
+++ lib/pages/favorite_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavoritePage extends StatelessWidget {
+  const FavoritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("FavoritePage")),
+      body: Center(child: Text("FavoritePage")),
+    );
+  }
+}
